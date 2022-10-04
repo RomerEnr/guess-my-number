6335e9a747811e4da4435832ae21ae2b29a40e3e
“Romer” <romeralvareziv@gmail.com>
2022-10-04 15:15:14 +0100

    Almost finished

--- src/index.js
+++ src/index.js
@@ -1,19 +1,55 @@
-"use strict";
-
-// document.querySelector(".message").innerText = "🎉 Correct number";
-// document.querySelector(".number").innerText = 20;
-// document.querySelector(".score").innerText = 4;
-// document.querySelector(".guess").value = 23;
+// Dom references
+const displayMessage = document.querySelector(".message");
 const guessButton = document.querySelector(".check");
-
-const handleGuessButton = () => {
-  const inputValue = document.querySelector(".guess").value;
-
-  if ((inputValue < 0 || inputValue > 20)) {
-    document.querySelector(".message").innerText = "🚫 Wrong value";
-    return false;
-  }
-  document.querySelector(".number").innerText = inputValue;
+const gameScore = document.querySelector(".score");
+const againButton = document.querySelector(".again");
+const inputValue = document.querySelector(".guess");
+const bigNumber = document.querySelector(".number");
+const rootStyles = document.documentElement.style;
+// Useful values
+const generateRandomValue = () => {
+  return Math.floor(Math.random() * 20) + 1;
 };
 
+let randomValue = generateRandomValue();
+let score = 20;
+gameScore.textContent = score;
+
+const handleGuessButton = () => {
+  const boxValue = Number(document.querySelector(".guess").value);
+
+  if ((boxValue < 0 || boxValue > 20)) {
+    displayMessage.textContent = "🚫 Wrong value";
+    return;
+  } else if (score === 0) {
+    displayMessage.textContent = "You actually lost";
+    rootStyles.setProperty("--background-color", "#c22222");
+  } else if (boxValue === randomValue) {
+    displayMessage.textContent = "🎉 Correct value";
+    rootStyles.setProperty("--background-color", "#60b347");
+  } else if (boxValue > randomValue) {
+    displayMessage.textContent = "Too high...";
+    score--;
+    gameScore.textContent = score;
+    gameScore.textContent = score;
+  } else if (boxValue < randomValue) {
+    displayMessage.textContent = "Too low...";
+    score -= 1;
+    gameScore.textContent = score;
+  }
+  document.querySelector(".number").innerText = boxValue;
+};
+
+const handleAgainButton = () => {
+  randomValue = generateRandomValue();
+  score = 20;
+  gameScore.textContent = score;
+  inputValue.value = "";
+  displayMessage.textContent = "Start guessing...";
+  bigNumber.textContent = "?";
+  rootStyles.setProperty("--background-color", "#222  ");
+};
+
+// Events
 guessButton.addEventListener("click", handleGuessButton);
+againButton.addEventListener("click", handleAgainButton);
